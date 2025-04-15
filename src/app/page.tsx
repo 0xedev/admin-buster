ccb8d48e87719f92d17e15b62abf75e90339b6ed
0xedev <oxdeve@gmail.com>
2025-04-15 15:13:29 +0100

    miniature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
-// src/app/page.tsx
-import { EnhancedPredictionMarketDashboard } from "@/components/enhanced-prediction-market-dashboard";
-import { Metadata } from "next";
+// // src/app/page.tsx
+// import { EnhancedPredictionMarketDashboard } from "";
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Forecast - Prediction Market",
-  description: "Forecast outcomes!",
-  openGraph: {
-    title: "Forecast",
-    images: ["/icon.jpg"],
-  },
-  other: {
-    "fc:frame": JSON.stringify({
-      version: "next",
-      imageUrl: "https://admin-buster.vercel.app/icon.jpg",
-      button: {
-        title: "Forecast",
-        action: {
-          type: "launch_frame",
-          name: "Forecast",
-          iconUrl: "https://admin-buster.vercel.app/icon1.jpg",
-          url: "https://buster-mkt.vercel.app",
-          splashImageUrl: "https://admin-buster.vercel.app/icon.jpg",
-          splashBackgroundColor: "#ffffff",
-        },
-      },
-    }),
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Forecast - Prediction Market",
+//   description: "Forecast outcomes!",
+//   openGraph: {
+//     title: "Forecast",
+//     images: ["/icon.jpg"],
+//   },
+//   other: {
+//     "fc:frame": JSON.stringify({
+//       version: "next",
+//       imageUrl: "https://admin-buster.vercel.app/icon.jpg",
+//       button: {
+//         title: "Forecast",
+//         action: {
+//           type: "launch_frame",
+//           name: "Forecast",
+//           iconUrl: "https://admin-buster.vercel.app/icon1.jpg",
+//           url: "https://buster-mkt.vercel.app",
+//           splashImageUrl: "https://admin-buster.vercel.app/icon.jpg",
+//           splashBackgroundColor: "#ffffff",
+//         },
+//       },
+//     }),
+//   },
+// };
 
-export default function Home() {
-  return <EnhancedPredictionMarketDashboard />;
-}
+// export default function Home() {
+//   return <EnhancedPredictionMarketDashboard />;
+// }
