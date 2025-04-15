87c1260dad64c36dbf36167db627c93e4a432f85
0xedev <oxdeve@gmail.com>
2025-04-15 15:33:18 +0100

    new setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,36 @@
-// // src/app/page.tsx
-// import { EnhancedPredictionMarketDashboard } from "";
-// import { Metadata } from "next";
+// src/app/page.tsx
 
-// export const metadata: Metadata = {
-//   title: "Forecast - Prediction Market",
-//   description: "Forecast outcomes!",
-//   openGraph: {
-//     title: "Forecast",
-//     images: ["/icon.jpg"],
-//   },
-//   other: {
-//     "fc:frame": JSON.stringify({
-//       version: "next",
-//       imageUrl: "https://admin-buster.vercel.app/icon.jpg",
-//       button: {
-//         title: "Forecast",
-//         action: {
-//           type: "launch_frame",
-//           name: "Forecast",
-//           iconUrl: "https://admin-buster.vercel.app/icon1.jpg",
-//           url: "https://buster-mkt.vercel.app",
-//           splashImageUrl: "https://admin-buster.vercel.app/icon.jpg",
-//           splashBackgroundColor: "#ffffff",
-//         },
-//       },
-//     }),
-//   },
-// };
+import { Metadata } from "next";
 
-// export default function Home() {
-//   return <EnhancedPredictionMarketDashboard />;
-// }
+export const metadata: Metadata = {
+  title: "Forecast - Prediction Market",
+  description: "Forecast outcomes!",
+  openGraph: {
+    title: "Forecast",
+    images: ["/icon.jpg"],
+  },
+  other: {
+    "fc:frame": JSON.stringify({
+      version: "next",
+      imageUrl: "https://admin-buster.vercel.app/icon.jpg",
+      button: {
+        title: "Forecast",
+        action: {
+          type: "launch_frame",
+          name: "Forecast",
+          iconUrl: "https://admin-buster.vercel.app/icon1.jpg",
+          url: "https://buster-mkt.vercel.app",
+          splashImageUrl: "https://admin-buster.vercel.app/icon.jpg",
+          splashBackgroundColor: "#ffffff",
+        },
+      },
+    }),
+  },
+};
+
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/admin");
+  return null;
+}
